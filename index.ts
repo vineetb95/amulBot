a3f1f08164d4938e889a43fc0c92c777a79d5ade
Vineet Bakshi <vineet@hypertest.co>
2025-08-01 13:01:16 +0530

    chore: update products
    Signed-off-by: Vineet Bakshi <vineet@hypertest.co>

--- index.ts
+++ index.ts
@@ -9,20 +9,18 @@ import express from 'express';
 let entryfileStartedMsgPromise = sendTelegramMessage('Bot process entryfile started running!');
 
 const productNames: string[] = [
-    // 'Amul Whey Protein Gift Pack, 32 g | Pack of 10 sachets',
     'Amul Whey Protein, 32 g | Pack of 30 Sachets',
     'Amul Whey Protein, 32 g | Pack of 60 Sachets',
-    // 'Amul Chocolate Whey Protein Gift Pack, 34 g | Pack of 10 sachets',
     'Amul Chocolate Whey Protein, 34 g | Pack of 30 sachets',
-    'Amul Chocolate Whey Protein, 34 g | Pack of 60 sachets',
-    'Amul High Protein Plain Lassi, 200 mL | Pack of 30',
-    'Amul High Protein Buttermilk, 200 mL | Pack of 30',
-    'Amul High Protein Rose Lassi, 200 mL | Pack of 30'
+    'Amul Chocolate Whey Protein, 34 g | Pack of 60 sachets'
 ];
 
 const bannedProducts: string[] = [
-    'Amul Whey Protein Gift Pack, 32 g | Pack of 10 sachets',
     'Amul Chocolate Whey Protein Gift Pack, 34 g | Pack of 10 sachets',
+    'Amul Chocolate Whey Protein Gift Pack, 34 g | Pack of 10 sachets',
+    'Amul High Protein Plain Lassi, 200 mL | Pack of 30',
+    'Amul High Protein Buttermilk, 200 mL | Pack of 30',
+    'Amul High Protein Rose Lassi, 200 mL | Pack of 30'
 ]
 
 let timeSinceEverythingWentOutOfStock = -1;
